8cb6087bad3ee88cad05cf60974ca55d8d7b9ce1
VasekJanoch <vena.janoch@windowslive.com>
2018-03-30 18:00:07 +0200

    Saving scene to .txt file.

---
+++ RayTracer/FileManipulator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer
+{
+    public class FileManipulator
+    {
+
+      public void saveSceneToTXT()
+      {
+            string outputText = SceneInfoContainer.toString();
+
+            if(SceneInfoContainer.shapes != null)
+            {
+
+                for(int i = 0; i < SceneInfoContainer.shapeCount; i++ )
+                {
+                    outputText += SceneInfoContainer.shapes[i].ToString() + "\r\n";
+
+                }
+            }
+
+            System.IO.File.WriteAllText(SceneInfoContainer.sceneOutputFilePath, outputText);
+
+
+        }
+
+    }
+}
